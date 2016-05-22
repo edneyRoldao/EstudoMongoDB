ef715379eab9bebd297946f58ab92b503cd17148
edneyRoldao <edneyroldao@gmail.com>
2016-05-22 02:35:47 -0300

    sobre referencias

---
+++ 06_referencias/criandoReferencias.js
@@ -0,0 +1,12 @@
+/*
+	Edney Roldão - 21/05/2016
+	- SOBRE REFERENCIAS
+
+	- O mongoDB não nos oferece uma maneira nativa de criarmos relacionamentos do tipo presente 
+	  no mundo de DB relacionais. Se quisermos realizar algo parecido, a responsabilidade pela criação
+	  e resolução desses relacionamentos será de nosso responsabilidade.
+
+	- Estudar sobre embedded document e DBRefs (database references)
+
+	- Existe uma forma de implementar esses relacionamentos com a API mongoose
+*/
